6b846f53540318cdb5ca4b8ea271f8cd2b8202f2
Andrey <kaspersky1236@gmail.com>
2026-08-04 20:26:29 +0400

    Sema: worked on DagnosticEngine class.

--- include/sema/sema.hpp
+++ include/sema/sema.hpp
@@ -3,6 +3,7 @@
 
 #include "parser/parser.hpp"
 #include <vector>
+#include <memory>
 
 struct Symbol {
     enum class Kind { GLOBAL, PARAM, LOCAL, UPVALUE };
@@ -13,20 +14,31 @@ struct Symbol {
 
 class ScopeStack {
 private:
-
     ScopeStack* parent_ = nullptr;
     std::unordered_map<std::string, Symbol> symbols_;
 
 public:
-    explicit ScopeStack(ScopeStack* p) : parent_(p) {} 
+    explicit ScopeStack(ScopeStack* p) : parent_(p) { } 
     ~ScopeStack() = default;
 
     void add_into_symbols(const std::string&, const Symbol&);
     bool has_locally(const std::string& name) const;
     Symbol* lookup(const std::string& name);
+    ScopeStack* get_parent();
 };
 
-struct DiagnosticEngine {};
+
+class DiagnosticEngine {
+private:
+    std::vector<std::string> diag_vect_;
+
+public:
+    enum class DiagType {ERROR, WARNING};
+    void collect_diags(const std::string&,
+                       const std::string&, 
+                       const DiagType&,
+                       const Node*);
+};
  
 class Visitor {
 public:
@@ -34,16 +46,44 @@ public:
 
     virtual void visit(UnaryOpNode*) = 0;
     virtual void visit(BinaryOpNode*) = 0;
-    // ...
+    virtual void visit(BitwiseNode*) = 0;
+    virtual void visit(MemberAccessNode*) = 0;
+    virtual void visit(BasicDataNode*) = 0;
+    virtual void visit(VariableNode*) = 0;
+    virtual void visit(VarWithAttributeNode*) = 0;
+    virtual void visit(DefineVariableNode*) = 0;
+    virtual void visit(MultipleVariableNode*) = 0;
+    virtual void visit(AndTernaryNode*) = 0;
+    virtual void visit(OrTernaryNode*) = 0;
+    virtual void visit(ArrayNode*) = 0;
+    virtual void visit(ExprWithIndexNode*) = 0;
+    virtual void visit(IndexNode*) = 0;
+    virtual void visit(XORNode*) = 0;
+    virtual void visit(TableFieldNode*) = 0;
+    virtual void visit(IfNode*) = 0;
+    virtual void visit(ElseIfNode*) = 0;
+    virtual void visit(DoNode*) = 0;
+    virtual void visit(WhileNode*) = 0;
+    virtual void visit(NumericForNode*) = 0;
+    virtual void visit(GenericForNode*) = 0;
+    virtual void visit(RepeatUntilNode*) = 0;
+    virtual void visit(FunctionNode*) = 0;
+    virtual void visit(AnonFunction*) = 0;
+    virtual void visit(MethodNode*) = 0;
+    virtual void visit(FunctionCallNode*) = 0;
+    virtual void visit(MethodCallNode*) = 0;
+    virtual void visit(ReturnNode*) = 0;
 };
 
 class SemanticAnalyzer : public Visitor {
 private:
-    std::vector<ScopeStack*> scopes;
-    DiagnosticEngine& diags;
+    std::vector<std::unique_ptr<ScopeStack>> scopes_;
+    DiagnosticEngine& diags_;
 
 public:
-    SemanticAnalyzer(DiagnosticEngine& d) : diags(d) { }
+    SemanticAnalyzer(DiagnosticEngine& d) : diags_(d) { 
+        scopes_.push_back(std::make_unique<ScopeStack>(nullptr));
+    }
 };
 
 #endif
--- src/sema/sema.cpp
+++ src/sema/sema.cpp
@@ -1,7 +1,16 @@
 #include "sema/sema.hpp"
+#include <format>
 
 void ScopeStack::add_into_symbols(const std::string& name, const Symbol& symbol) {
     symbols_[name] = symbol;
+}
+
+bool ScopeStack::has_locally(const std::string& name) const {
+    return symbols_.find(name) != symbols_.end();
+}
+
+ScopeStack* ScopeStack::get_parent() {
+    return this->parent_;
 }
 
 Symbol* ScopeStack::lookup(const std::string& name) {
@@ -18,6 +27,22 @@ Symbol* ScopeStack::lookup(const std::string& name) {
     return nullptr;
 }
 
-bool ScopeStack::has_locally(const std::string& name) const {
-    return symbols_.find(name) != symbols_.end();
+void DiagnosticEngine::collect_diags(
+    const std::string& message,
+    const std::string& name,
+    const DiagType& diag,
+    const Node* node) 
+{
+    std::string diag_str;
+
+    if (diag == DiagType::ERROR) diag_str = "Error";
+    else diag_str = "Warning";
+
+    std::string formated_mes = std::format("[{}] {} - {} at line {}, column {}.", 
+                                           diag_str,
+                                           message, name, 
+                                           node->position.y,
+                                           node->position.x);
+
+    diag_vect_.push_back(formated_mes);
 }
